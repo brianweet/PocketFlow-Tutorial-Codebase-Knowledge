d1d3d2877715c3986e3b3dd4af349a0dd5197ef9
Brian Weeteling <brianweeteling@gmail.com>
2025-05-08 14:42:25 +0200

    Try gemini 2.5 flash

--- utils/call_llm.py
+++ utils/call_llm.py
@@ -21,68 +21,82 @@ logger.addHandler(file_handler)
 cache_file = "llm_cache.json"
 
 # By default, we Google Gemini 2.5 pro, as it shows great performance for code understanding
-# def call_llm(prompt: str, use_cache: bool = True) -> str:
-#     # Log the prompt
-#     logger.info(f"PROMPT: {prompt}")
+def call_llm(prompt: str, use_cache: bool = True) -> str:
+    # Log the prompt
+    logger.info(f"PROMPT: {prompt}")
     
-#     # Check cache if enabled
-#     if use_cache:
-#         # Load cache from disk
-#         cache = {}
-#         if os.path.exists(cache_file):
-#             try:
-#                 with open(cache_file, 'r') as f:
-#                     cache = json.load(f)
-#             except:
-#                 logger.warning(f"Failed to load cache, starting with empty cache")
+    # Check cache if enabled
+    if use_cache:
+        # Load cache from disk
+        cache = {}
+        if os.path.exists(cache_file):
+            try:
+                with open(cache_file, 'r') as f:
+                    cache = json.load(f)
+            except:
+                logger.warning(f"Failed to load cache, starting with empty cache")
         
-#         # Return from cache if exists
-#         if prompt in cache:
-#             logger.info(f"RESPONSE: {cache[prompt]}")
-#             return cache[prompt]
+        # Return from cache if exists
+        if prompt in cache:
+            logger.info(f"RESPONSE: {cache[prompt]}")
+            return cache[prompt]
     
-#     # Call the LLM if not in cache or cache disabled
-#     client = genai.Client(
-#         # vertexai=True, 
-#         # TODO: change to your own project id and location
-#         # project=os.getenv("GEMINI_PROJECT_ID", "your-project-id"),
-#         # location=os.getenv("GEMINI_LOCATION", "us-central1"),
-#         api_key=os.getenv("GEMINI_API_KEY", "your-api_key"),
-#     )
-#     # You can comment the previous line and use the AI Studio key instead:
-#     # client = genai.Client(
-#     #     api_key=os.getenv("GEMINI_API_KEY", "your-api_key"),
-#     # )
-#     model = os.getenv("GEMINI_MODEL", "gemini-2.5-pro-exp-03-25")
-#     response = client.models.generate_content(
-#         model=model,
-#         contents=[prompt]
-#     )
-#     response_text = response.text
+    # Call the LLM if not in cache or cache disabled
+    client = genai.Client(
+        # vertexai=True, 
+        # TODO: change to your own project id and location
+        # project=os.getenv("GEMINI_PROJECT_ID", "your-project-id"),
+        # location=os.getenv("GEMINI_LOCATION", "us-central1"),
+        api_key=os.getenv("GEMINI_API_KEY", "your-api_key"),
+    )
+    # You can comment the previous line and use the AI Studio key instead:
+    # client = genai.Client(
+    #     api_key=os.getenv("GEMINI_API_KEY", "your-api_key"),
+    # )
+
+    # model = os.getenv("GEMINI_MODEL", "gemini-2.5-pro-exp-03-25")
+    # response = client.models.generate_content(
+    #     model=model,
+    #     contents=[prompt]
+    # )
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-preview-04-17",
+        contents=[prompt],
+        config=genai.types.GenerateContentConfig(
+                thinking_config=genai.types.ThinkingConfig(
+                    thinking_budget=1024
+                )
+            )
+    )
+
+    print(response.text)
+
+    response_text = response.text
     
-#     # Log the response
-#     logger.info(f"RESPONSE: {response_text}")
+    # Log the response
+    logger.info(f"RESPONSE: {response_text}")
     
-#     # Update cache if enabled
-#     if use_cache:
-#         # Load cache again to avoid overwrites
-#         cache = {}
-#         if os.path.exists(cache_file):
-#             try:
-#                 with open(cache_file, 'r') as f:
-#                     cache = json.load(f)
-#             except:
-#                 pass
+    # Update cache if enabled
+    if use_cache:
+        # Load cache again to avoid overwrites
+        cache = {}
+        if os.path.exists(cache_file):
+            try:
+                with open(cache_file, 'r') as f:
+                    cache = json.load(f)
+            except:
+                pass
         
-#         # Add to cache and save
-#         cache[prompt] = response_text
-#         try:
-#             with open(cache_file, 'w') as f:
-#                 json.dump(cache, f)
-#         except Exception as e:
-#             logger.error(f"Failed to save cache: {e}")
+        # Add to cache and save
+        cache[prompt] = response_text
+        try:
+            with open(cache_file, 'w') as f:
+                json.dump(cache, f)
+        except Exception as e:
+            logger.error(f"Failed to save cache: {e}")
     
-#     return response_text
+    return response_text
 
 # # Use Anthropic Claude 3.7 Sonnet Extended Thinking
 # def call_llm(prompt, use_cache: bool = True):
@@ -117,74 +131,74 @@ cache_file = "llm_cache.json"
 #     return r.choices[0].message.content
 
 # Use Github Models - azure.ai
-def call_llm(prompt, use_cache: bool = True):
-    from azure.ai.inference import ChatCompletionsClient
-    from azure.ai.inference.models import SystemMessage, UserMessage
-    from azure.core.credentials import AzureKeyCredential
+# def call_llm(prompt, use_cache: bool = True):
+#     from azure.ai.inference import ChatCompletionsClient
+#     from azure.ai.inference.models import SystemMessage, UserMessage
+#     from azure.core.credentials import AzureKeyCredential
 
-    endpoint = "https://models.github.ai/inference"
-    model = os.environ.get("GITHUB_MODEL", "openai/o3-mini")
-    token =  os.environ.get("GITHUB_TOKEN", "your-api-key")
+#     endpoint = "https://models.github.ai/inference"
+#     model = os.environ.get("GITHUB_MODEL", "openai/o3-mini")
+#     token =  os.environ.get("GITHUB_TOKEN", "your-api-key")
 
-    # Validate the token
-    if not token or token == "your-api-key":
-        raise ValueError("Please set the GITHUB_TOKEN environment variable to use the GitHub Models API.")
+#     # Validate the token
+#     if not token or token == "your-api-key":
+#         raise ValueError("Please set the GITHUB_TOKEN environment variable to use the GitHub Models API.")
 
-    client = ChatCompletionsClient(
-        endpoint=endpoint,
-        credential=AzureKeyCredential(token),
-    )
+#     client = ChatCompletionsClient(
+#         endpoint=endpoint,
+#         credential=AzureKeyCredential(token),
+#     )
 
-    response = client.complete(
-        messages=[
-            # SystemMessage(""),
-            {"role": "developer", "content": ""},
-            UserMessage(prompt)
-        ],
-        # temperature=1,
-        # top_p=1,
-        model=model
-    )
+#     response = client.complete(
+#         messages=[
+#             # SystemMessage(""),
+#             {"role": "developer", "content": ""},
+#             UserMessage(prompt)
+#         ],
+#         # temperature=1,
+#         # top_p=1,
+#         model=model
+#     )
 
-    return response.choices[0].message.content
+#     return response.choices[0].message.content
 
-# Use Github Models - openai
-def call_llm(prompt, use_cache: bool = True):
-    from openai import OpenAI
+# # Use Github Models - openai
+# def call_llm(prompt, use_cache: bool = True):
+#     from openai import OpenAI
 
-    endpoint = "https://models.github.ai/inference"
-    model = os.environ.get("GITHUB_MODEL", "openai/o3-mini")
-    token =  os.environ.get("GITHUB_TOKEN", "your-api-key")
+#     endpoint = "https://models.github.ai/inference"
+#     model = os.environ.get("GITHUB_MODEL", "openai/o3-mini")
+#     token =  os.environ.get("GITHUB_TOKEN", "your-api-key")
 
-    # Validate the token
-    if not token or token == "your-api-key":
-        raise ValueError("Please set the GITHUB_TOKEN environment variable to use the GitHub Models API.")
+#     # Validate the token
+#     if not token or token == "your-api-key":
+#         raise ValueError("Please set the GITHUB_TOKEN environment variable to use the GitHub Models API.")
     
-    logger.info(f"PROMPT: {prompt}")
-    logger.info(f"MODEL: {model}")
+#     logger.info(f"PROMPT: {prompt}")
+#     logger.info(f"MODEL: {model}")
 
-    client = OpenAI(
-        base_url=endpoint,
-        api_key=token,
-    )
+#     client = OpenAI(
+#         base_url=endpoint,
+#         api_key=token,
+#     )
 
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "developer",
-                "content": "",
-            },
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        model=model
-    )
+#     response = client.chat.completions.create(
+#         messages=[
+#             {
+#                 "role": "developer",
+#                 "content": "",
+#             },
+#             {
+#                 "role": "user",
+#                 "content": prompt,
+#             }
+#         ],
+#         model=model
+#     )
 
-    print(response.choices[0].message.content)
+#     print(response.choices[0].message.content)
 
-    return response.choices[0].message.content
+#     return response.choices[0].message.content
 
 if __name__ == "__main__":
     test_prompt = "Hello, how are you?"
